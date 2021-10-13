269a979f2ef73f7bdd7b1fe12ec34013fe6d08bd
becky sama <samarebecca616@gmail.com>
2021-10-13 16:23:47 +0100

    Add serialize and deserialize method

--- MyLibs.Serialisation/Serializer.cs
+++ MyLibs.Serialisation/Serializer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace MyLibs.Serialisation
 {
     public class Serializer
     {
+        public void Serialize<T>(T data, string parth)
+        {
+            using (StreamWriter file=new StreamWriter(parth, true))
+            {
+                BinaryFormatter bf =new BinaryFormatter();
+                bf.Serialize(file.BaseStream, data);
+            }
+        }
+        public T Deserialize<T>(string parth)
+        {
+            using (StreamWriter file = new StreamWriter(parth, true))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                return(T) bf.Deserialize(file.BaseStream);
+            }
+        }
     }
 }
